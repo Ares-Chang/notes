642cde7ce8a084c9c4509a65382b4a67381bfddb
Ares-Chang <2622310479@qq.com>
2020-02-09 23:16:49 +0800

    chore: 更改启动命令

--- config/sidebar/about/index.js
+++ config/sidebar/about/index.js
@@ -3,5 +3,5 @@ const utils = require('../../../utils')
 const children = ['GitHub Pages', 'GitHub Actions 自动部署']
 
 module.exports = [
-	utils.gneSidebar('GitHub 部署', children)
+	utils.gneSidebar('GitHub 部署', children,false)
 ]
