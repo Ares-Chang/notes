512f73d0849212bc6597f88763844786dd06acba
Ares-Chang <2622310479@qq.com>
2020-02-10 15:38:46 +0800

    add: nrm

--- config/sidebar/hodgepodge/index.js
+++ config/sidebar/hodgepodge/index.js
@@ -1,6 +1,6 @@
 const utils = require('../../../utils')
 
-const children = ['', 'VueCLI-download/']
+const children = ['', 'VueCLI-download/', 'nrm/']
 
 module.exports = [
 	utils.gneSidebar('大杂绘', children, false)
