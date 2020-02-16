e5fa6d49d6302829e2c1771b2cc2c77701fc3934
Ares-Chang <2622310479@qq.com>
2020-02-16 21:56:30 +0800

    add: 安利 Cmder

--- config/sidebar/tools/index.js
+++ config/sidebar/tools/index.js
@@ -1,6 +1,6 @@
 const utils = require('../../../utils')
 
-const children = ['', 'Plugin/']
+const children = ['', 'Sublime-Plugin/', 'Cmder/']
 
 module.exports = [
 	utils.gneSidebar('工具', children, false)
