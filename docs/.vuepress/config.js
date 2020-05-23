6dda254638037a6b271616f18013ccc6ed5fc931
Ares-Chang <2622310479@qq.com>
2020-05-23 23:43:54 +0800

    feat: 插件更新应用,项目目录整理
    1. 添加 @vuepress/back-to-top,增加回到顶部功能
    2. 添加图片缩放功能
    3. 添加页面滚动自动对应左侧标题
    4. 项目目录整理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,9 @@
  * 网站整体部署
  */
 
-const nav = require('../../config/nav')
-const sidebar = require('../../config/sidebar')
+const nav = require('./config/nav')
+const sidebar = require('./config/sidebar')
+const plugins = require('./config/plugins')
 
 module.exports = {
   title: 'Ares Chang',  // 网站名字
@@ -16,19 +17,17 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/logo.ico' }]  // 网站icon图标
   ],
-  // base: '/notes/', // 基础路径设置
+  plugins, // 各种插件引入
+  // base: '/notes/', // 基础路径设置,部署在 github 需要打开
   themeConfig: {
     repo: 'Ares-Chang/notes', // 添加一个GitH链接
     // 文档放置在master分支下，要编辑需要重新定向：
     editLinks: true,  // 启用左下角编辑文档标签
     editLinkText: '编辑文档!', // 页面左下角提示
-    docsDir: 'docs', // 如文档不是放在仓库的根目录下需要设置 否则无法编辑
+    docsDir: 'docs', // 如文档不是放在仓库的根目录下需要设置 否则无法编辑
     lastUpdated: '上次更新',  // 更新时间
-    // 导航栏配置
-    nav,
-    // 侧边栏配置
-    sidebar,
-    // 标题深度
-    sidebarDepth: 2
+    nav, // 导航栏配置
+    sidebar, // 侧边栏配置
+    sidebarDepth: 2 // 标题深度
   }
 }
---
+++ docs/.vuepress/config/plugins/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+	// 回到顶部插件
+	'@vuepress/back-to-top': true,
+	// 图片缩放插件
+	'@vuepress/medium-zoom': {
+		selector: 'img.zoom',
+		options: {
+			margin: 16
+		}
+	},
+	// 页面滚动自动对应左侧标题
+	'@vuepress/active-header-links': true
+}
